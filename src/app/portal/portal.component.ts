9098e9b88c4467849df029bb0ccdeba275d0fd98
Bhumi Goyal <bgoyal2003@gmail.com>
2024-07-27 00:08:58 +0530

    portal

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-portal',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.css'
 })
